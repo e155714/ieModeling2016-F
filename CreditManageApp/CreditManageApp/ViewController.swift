a8e28f27426a0bbf688297124f9e7b3c0147745b
Intel0tw5727 <e155727.ha1cyon@gmail.com>
2016-11-23 21:25:53 +0900

    first commit and create testApp

---
+++ CreditManageApp/CreditManageApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  CreditManageApp
+//
+//  Created by Shingo Tawada on 2016/11/23.
+//  Copyright © 2016年 ieModeling2016-F. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        label.text = "Hello Resurretion of F!!"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
